d4efd62b80f1ca82d133e9d7fa8490fd7442987c
gimbyeong-gwan <ansd_my3073@naver.com>
2025-11-11 14:51:27 +0900

    feat: 13~14 search, 필터 기능 추가

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,16 +1,25 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useNavigate } from 'react-router-dom';
 
 const AppLayout = () => {
+    const [keyword, setKeyword] = useState('');
+    const navigate = useNavigate()
+    const searchByKeyword = (event) => {
+        event.preventDefault();
+        // url 바꿔준다.
+        navigate(`/movies?q=${keyword}`);
+        setKeyword('');
+    }
   return (
     <div>
         <Navbar expand="lg" bg="dark" data-bs-theme="dark" style={{ backgroundColor: '#141414' }}>
             <Container fluid>
-                <Navbar.Brand href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix" height="24"/></Navbar.Brand>
+                <Navbar.Brand href="/"><img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix" height="24"/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
@@ -22,14 +31,16 @@ const AppLayout = () => {
                         <Nav.Link as={Link} to="/movies">Movies</Nav.Link>
                     
                     </Nav>
-                    <Form className="d-flex">
+                    <Form className="d-flex" onSubmit={searchByKeyword}>
                         <Form.Control
                             type="search"
                             placeholder="Search"
                             className="me-2 bg-dark text-light border-secondary"
                             aria-label="Search"
+                            value={keyword}
+                            onChange={(event) => setKeyword(event.target.value)}
                         />
-                        <Button variant="outline-light">Search</Button>
+                        <Button variant="outline-danger" type='submit'>Search</Button>
                     </Form>
                 </Navbar.Collapse>
             </Container>
